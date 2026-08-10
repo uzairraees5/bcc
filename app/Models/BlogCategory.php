6b2c538dea2bdf4743ca67c36b059d0231003c1d
uzairraees5 <81719403+uzairraees5@users.noreply.github.com>
2026-08-10 19:53:49 +0500

    Expand blog category SEO fields

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -7,17 +7,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class BlogCategory extends Model
 {
-    protected $fillable = [
-        'name',
-        'slug',
-        'description',
-        'seo_title',
-        'meta_description',
-        'is_active',
-    ];
-
-    public function posts(): HasMany
-    {
-        return $this->hasMany(BlogPost::class, 'category_id');
-    }
+    protected $fillable = ['name','slug','description','seo_title','meta_description','canonical_url','robots_index','robots_follow','is_active'];
+    protected $casts = ['robots_index' => 'boolean','robots_follow' => 'boolean','is_active' => 'boolean'];
+    public function posts(): HasMany { return $this->hasMany(BlogPost::class, 'category_id'); }
 }
