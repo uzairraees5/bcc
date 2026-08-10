ac57d420332649db02c6a89f6dd42392ea2f5676
uzairraees5 <81719403+uzairraees5@users.noreply.github.com>
2026-08-10 19:52:52 +0500

    Register SEO authorization middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'auth' => \App\Http\Middleware\Authenticate::class,
             'admin' => \App\Http\Middleware\EnsureAdmin::class,
+            'seo.admin' => \App\Http\Middleware\EnsureSeoAdmin::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
